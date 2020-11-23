90f04dfd370a8930755a3291dcea839b74ca39ad
Luiz Gomes <luiz.gomes@spro.com.br>
2020-11-23 00:36:05 -0300

    mongodb push

--- src/controllers/ClickController.js
+++ src/controllers/ClickController.js
@@ -7,7 +7,7 @@ module.exports = {
         let data    = req.body
         let query   = {"heatMaps.url" : data.url}
      
-        Projeto.findOneAndUpdate(query,  {$set: {"heatMaps.$.clicks": data.clicks}}, (error, projeto) => {
+        Projeto.findOneAndUpdate(query,  {$push: {"heatMaps.$.clicks": data.clicks}}, (error, projeto) => {
             if(error)  return res.status(400).json(error)
             return res.status(400).json(projeto)
         })
@@ -18,7 +18,7 @@ module.exports = {
         let query =  {"heatMaps.url" : data.url}
         console.log(query)
         console.log(data.screenshot[0].date)
-        Projeto.findOneAndUpdate(query,  {$set: {"heatMaps.$.screenshot": data.screenshot}}, (error, projeto) => {
+        Projeto.findOneAndUpdate(query,  {$push: {"heatMaps.$.screenshot": data.screenshot}}, (error, projeto) => {
             console.log(error)
             console.log(projeto)
             if(error)  return res.status(400).json(error)
